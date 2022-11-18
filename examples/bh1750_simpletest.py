be3728434955a63373e66befe495008711c2ac05
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:21 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/bh1750_simpletest.py
+++ examples/bh1750_simpletest.py
@@ -5,7 +5,8 @@ import time
 import board
 import adafruit_bh1750
 
-i2c = board.I2C()
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_bh1750.BH1750(i2c)
 
 while True:
